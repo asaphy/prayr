c8b5d0885b603b6516ec3dc740d3d819fd1294ce
Asaph Yuan <asaphy@bu.edu>
2014-12-28 22:11:42 -0800

    added lists and prompt for name

--- Android/Prayr/app/src/main/java/com/asaphyuan/prayr/MainActivity.java
+++ Android/Prayr/app/src/main/java/com/asaphyuan/prayr/MainActivity.java
@@ -1,19 +1,39 @@
 package com.asaphyuan.prayr;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener  {
 
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
 
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +49,26 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        // 3. Access the EditText defined in layout XML
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+
+        // 5. Set this activity to react to list items being pressed
+        mainListView.setOnItemClickListener(this);
+
+        // 6. Greet the user, or ask for their name if new
+        displayWelcome();
     }
 
 
@@ -41,7 +81,73 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        // Test the Button
-        mainTextView.setText("Button pressed!");
+        // Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position + ": " + mNameList.get(position));
+    }
+
+    public void displayWelcome() {
+
+        // Access the device's key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        // Read the user's name,
+        // or an empty string if nothing found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+
+            // If the name is valid, display a Toast welcoming them
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        } else {
+
+        // otherwise, show a dialog to ask for their name
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Hello!");
+        alert.setMessage("What is your name?");
+
+        // Create EditText for entry
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        // Make an "OK" button to save the name
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                // Grab the EditText's input
+                String inputName = input.getText().toString();
+
+                // Put it into memory (don't forget to commit!)
+                SharedPreferences.Editor e = mSharedPreferences.edit();
+                e.putString(PREF_NAME, inputName);
+                e.commit();
+
+                // Welcome the new user
+                Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Make a "Cancel" button
+        // that simply dismisses the alert
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {}
+        });
+
+        alert.show();
+        }
     }
 }
